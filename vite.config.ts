92133f6d06e483306bfe5250ae23e6945766253b
qianlifeng <qianlf2010@gmail.com>
2023-08-27 21:39:51 +0800

    Add mkcert to local https preview

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
 import tsconfigPaths from "vite-tsconfig-paths"
+import mkcert from "vite-plugin-mkcert"
 
 export default defineConfig(({ mode }) => ({
+	server: {
+		https: true
+	},
 	plugins: [
+		mkcert(),
 		tsconfigPaths(),
 		react(),
 		...(mode === "test"
@@ -14,9 +19,12 @@ export default defineConfig(({ mode }) => ({
 					eslintPlugin(),
 					VitePWA({
 						registerType: "autoUpdate",
+						devOptions: {
+							enabled: true
+						},
 						includeAssets: ["favicon.png", "robots.txt", "apple-touch-icon.png", "icons/*.svg", "fonts/*.woff2"],
 						manifest: {
-							theme_color: "#BD34FE",
+							theme_color: "#121212",
 							icons: [
 								{
 									src: "/android-chrome-192x192.png",
